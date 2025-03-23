667795864fea95ebf272d1dd9d03f91ce509fa95
DevonArtmeier <devon.artmeier@outlook.com>
2025-03-23 14:21:05 -0400

    Parepare intermediary binary format for sprites (for easy use with tools)

--- mars/sprite_data.asm
+++ mars/sprite_data.asm
@@ -3,319 +3,319 @@
 ; -------------------------------------------------------------------------
 
 MarsSpr_000:
-	include	"mars/sprites/000.asm"
+	include	"mars/sprites/converted/000.asm"
 	align	4,$FF
 MarsSpr_001:
-	include	"mars/sprites/001.asm"
+	include	"mars/sprites/converted/001.asm"
 	align	4,$FF
 MarsSpr_002:
-	include	"mars/sprites/002.asm"
+	include	"mars/sprites/converted/002.asm"
 	align	4,$FF
 MarsSpr_003:
-	include	"mars/sprites/003.asm"
+	include	"mars/sprites/converted/003.asm"
 	align	4,$FF
 MarsSpr_004:
-	include	"mars/sprites/004.asm"
+	include	"mars/sprites/converted/004.asm"
 	align	4,$FF
 MarsSpr_005:
-	include	"mars/sprites/005.asm"
+	include	"mars/sprites/converted/005.asm"
 	align	4,$FF
 MarsSpr_006:
-	include	"mars/sprites/006.asm"
+	include	"mars/sprites/converted/006.asm"
 	align	4,$FF
 MarsSpr_007:
-	include	"mars/sprites/007.asm"
+	include	"mars/sprites/converted/007.asm"
 	align	4,$FF
 MarsSpr_008:
-	include	"mars/sprites/008.asm"
+	include	"mars/sprites/converted/008.asm"
 	align	4,$FF
 MarsSpr_009:
-	include	"mars/sprites/009.asm"
+	include	"mars/sprites/converted/009.asm"
 	align	4,$FF
 MarsSpr_010:
-	include	"mars/sprites/010.asm"
+	include	"mars/sprites/converted/010.asm"
 	align	4,$FF
 MarsSpr_011:
-	include	"mars/sprites/011.asm"
+	include	"mars/sprites/converted/011.asm"
 	align	4,$FF
 MarsSpr_012:
-	include	"mars/sprites/012.asm"
+	include	"mars/sprites/converted/012.asm"
 	align	4,$FF
 MarsSpr_013:
-	include	"mars/sprites/013.asm"
+	include	"mars/sprites/converted/013.asm"
 	align	4,$FF
 MarsSpr_014:
-	include	"mars/sprites/014.asm"
+	include	"mars/sprites/converted/014.asm"
 	align	4,$FF
 MarsSpr_015:
-	include	"mars/sprites/015.asm"
+	include	"mars/sprites/converted/015.asm"
 	align	4,$FF
 MarsSpr_017:
-	include	"mars/sprites/017.asm"
+	include	"mars/sprites/converted/017.asm"
 	align	4,$FF
 MarsSpr_018:
-	include	"mars/sprites/018.asm"
+	include	"mars/sprites/converted/018.asm"
 	align	4,$FF
 MarsSpr_019:
-	include	"mars/sprites/019.asm"
+	include	"mars/sprites/converted/019.asm"
 	align	4,$FF
 MarsSpr_020:
-	include	"mars/sprites/020.asm"
+	include	"mars/sprites/converted/020.asm"
 	align	4,$FF
 MarsSpr_022:
-	include	"mars/sprites/022.asm"
+	include	"mars/sprites/converted/022.asm"
 	align	4,$FF
 MarsSpr_021:
-	include	"mars/sprites/021.asm"
+	include	"mars/sprites/converted/021.asm"
 	align	4,$FF
 MarsSpr_023:
-	include	"mars/sprites/023.asm"
+	include	"mars/sprites/converted/023.asm"
 	align	4,$FF
 MarsSpr_024:
-	include	"mars/sprites/024.asm"
+	include	"mars/sprites/converted/024.asm"
 	align	4,$FF
 MarsSpr_025:
-	include	"mars/sprites/025.asm"
+	include	"mars/sprites/converted/025.asm"
 	align	4,$FF
 MarsSpr_026:
-	include	"mars/sprites/026.asm"
+	include	"mars/sprites/converted/026.asm"
 	align	4,$FF
 MarsSpr_027:
-	include	"mars/sprites/027.asm"
+	include	"mars/sprites/converted/027.asm"
 	align	4,$FF
 MarsSpr_028:
-	include	"mars/sprites/028.asm"
+	include	"mars/sprites/converted/028.asm"
 	align	4,$FF
 MarsSpr_029:
-	include	"mars/sprites/029.asm"
+	include	"mars/sprites/converted/029.asm"
 	align	4,$FF
 MarsSpr_031:
-	include	"mars/sprites/031.asm"
+	include	"mars/sprites/converted/031.asm"
 	align	4,$FF
 MarsSpr_032:
-	include	"mars/sprites/032.asm"
+	include	"mars/sprites/converted/032.asm"
 	align	4,$FF
 MarsSpr_033:
-	include	"mars/sprites/033.asm"
+	include	"mars/sprites/converted/033.asm"
 	align	4,$FF
 MarsSpr_034:
-	include	"mars/sprites/034.asm"
+	include	"mars/sprites/converted/034.asm"
 	align	4,$FF
 MarsSpr_035:
-	include	"mars/sprites/035.asm"
+	include	"mars/sprites/converted/035.asm"
 	align	4,$FF
 MarsSpr_036:
-	include	"mars/sprites/036.asm"
+	include	"mars/sprites/converted/036.asm"
 	align	4,$FF
 MarsSpr_037:
-	include	"mars/sprites/037.asm"
+	include	"mars/sprites/converted/037.asm"
 	align	4,$FF
 MarsSpr_038:
-	include	"mars/sprites/038.asm"
+	include	"mars/sprites/converted/038.asm"
 	align	4,$FF
 MarsSpr_039:
-	include	"mars/sprites/039.asm"
+	include	"mars/sprites/converted/039.asm"
 	align	4,$FF
 MarsSpr_040:
-	include	"mars/sprites/040.asm"
+	include	"mars/sprites/converted/040.asm"
 	align	4,$FF
 MarsSpr_042:
-	include	"mars/sprites/042.asm"
+	include	"mars/sprites/converted/042.asm"
 	align	4,$FF
 MarsSpr_041:
-	include	"mars/sprites/041.asm"
+	include	"mars/sprites/converted/041.asm"
 	align	4,$FF
 MarsSpr_043:
-	include	"mars/sprites/043.asm"
+	include	"mars/sprites/converted/043.asm"
 	align	4,$FF
 MarsSpr_044:
-	include	"mars/sprites/044.asm"
+	include	"mars/sprites/converted/044.asm"
 	align	4,$FF
 MarsSpr_045:
-	include	"mars/sprites/045.asm"
+	include	"mars/sprites/converted/045.asm"
 	align	4,$FF
 MarsSpr_046:
-	include	"mars/sprites/046.asm"
+	include	"mars/sprites/converted/046.asm"
 	align	4,$FF
 MarsSpr_047:
-	include	"mars/sprites/047.asm"
+	include	"mars/sprites/converted/047.asm"
 	align	4,$FF
 MarsSpr_048:
-	include	"mars/sprites/048.asm"
+	include	"mars/sprites/converted/048.asm"
 	align	4,$FF
 MarsSpr_049:
-	include	"mars/sprites/049.asm"
+	include	"mars/sprites/converted/049.asm"
 	align	4,$FF
 MarsSpr_050:
-	include	"mars/sprites/050.asm"
+	include	"mars/sprites/converted/050.asm"
 	align	4,$FF
 MarsSpr_051:
-	include	"mars/sprites/051.asm"
+	include	"mars/sprites/converted/051.asm"
 	align	4,$FF
 MarsSpr_052:
-	include	"mars/sprites/052.asm"
+	include	"mars/sprites/converted/052.asm"
 	align	4,$FF
 MarsSpr_053:
-	include	"mars/sprites/053.asm"
+	include	"mars/sprites/converted/053.asm"
 	align	4,$FF
 MarsSpr_054:
-	include	"mars/sprites/054.asm"
+	include	"mars/sprites/converted/054.asm"
 	align	4,$FF
 MarsSpr_055:
-	include	"mars/sprites/055.asm"
+	include	"mars/sprites/converted/055.asm"
 	align	4,$FF
 MarsSpr_056:
-	include	"mars/sprites/056.asm"
+	include	"mars/sprites/converted/056.asm"
 	align	4,$FF
 MarsSpr_059:
-	include	"mars/sprites/059.asm"
+	include	"mars/sprites/converted/059.asm"
 	align	4,$FF
 MarsSpr_060:
-	include	"mars/sprites/060.asm"
+	include	"mars/sprites/converted/060.asm"
 	align	4,$FF
 MarsSpr_061:
-	include	"mars/sprites/061.asm"
+	include	"mars/sprites/converted/061.asm"
 	align	4,$FF
 MarsSpr_062:
-	include	"mars/sprites/062.asm"
+	include	"mars/sprites/converted/062.asm"
 	align	4,$FF
 MarsSpr_063:
-	include	"mars/sprites/063.asm"
+	include	"mars/sprites/converted/063.asm"
 	align	4,$FF
 MarsSpr_064:
-	include	"mars/sprites/064.asm"
+	include	"mars/sprites/converted/064.asm"
 	align	4,$FF
 MarsSpr_065:
-	include	"mars/sprites/065.asm"
+	include	"mars/sprites/converted/065.asm"
 	align	4,$FF
 MarsSpr_066:
-	include	"mars/sprites/066.asm"
+	include	"mars/sprites/converted/066.asm"
 	align	4,$FF
 MarsSpr_067:
-	include	"mars/sprites/067.asm"
+	include	"mars/sprites/converted/067.asm"
 	align	4,$FF
 MarsSpr_068:
-	include	"mars/sprites/068.asm"
+	include	"mars/sprites/converted/068.asm"
 	align	4,$FF
 MarsSpr_069:
-	include	"mars/sprites/069.asm"
+	include	"mars/sprites/converted/069.asm"
 	align	4,$FF
 MarsSpr_070:
-	include	"mars/sprites/070.asm"
+	include	"mars/sprites/converted/070.asm"
 	align	4,$FF
 MarsSpr_071:
-	include	"mars/sprites/071.asm"
+	include	"mars/sprites/converted/071.asm"
 	align	4,$FF
 MarsSpr_072:
-	include	"mars/sprites/072.asm"
+	include	"mars/sprites/converted/072.asm"
 	align	4,$FF
 MarsSpr_073:
-	include	"mars/sprites/073.asm"
+	include	"mars/sprites/converted/073.asm"
 	align	4,$FF
 MarsSpr_074:
-	include	"mars/sprites/074.asm"
+	include	"mars/sprites/converted/074.asm"
 	align	4,$FF
 MarsSpr_077:
-	include	"mars/sprites/077.asm"
+	include	"mars/sprites/converted/077.asm"
 	align	4,$FF
 MarsSpr_078:
-	include	"mars/sprites/078.asm"
+	include	"mars/sprites/converted/078.asm"
 	align	4,$FF
 MarsSpr_079:
-	include	"mars/sprites/079.asm"
+	include	"mars/sprites/converted/079.asm"
 	align	4,$FF
 MarsSpr_080:
-	include	"mars/sprites/080.asm"
+	include	"mars/sprites/converted/080.asm"
 	align	4,$FF
 MarsSpr_081:
-	include	"mars/sprites/081.asm"
+	include	"mars/sprites/converted/081.asm"
 	align	4,$FF
 MarsSpr_082:
-	include	"mars/sprites/082.asm"
+	include	"mars/sprites/converted/082.asm"
 	align	4,$FF
 MarsSpr_083:
-	include	"mars/sprites/083.asm"
+	include	"mars/sprites/converted/083.asm"
 	align	4,$FF
 MarsSpr_084:
-	include	"mars/sprites/084.asm"
+	include	"mars/sprites/converted/084.asm"
 	align	4,$FF
 MarsSpr_085:
-	include	"mars/sprites/085.asm"
+	include	"mars/sprites/converted/085.asm"
 	align	4,$FF
 MarsSpr_086:
-	include	"mars/sprites/086.asm"
+	include	"mars/sprites/converted/086.asm"
 	align	4,$FF
 MarsSpr_087:
-	include	"mars/sprites/087.asm"
+	include	"mars/sprites/converted/087.asm"
 	align	4,$FF
 MarsSpr_088:
-	include	"mars/sprites/088.asm"
+	include	"mars/sprites/converted/088.asm"
 	align	4,$FF
 MarsSpr_089:
-	include	"mars/sprites/089.asm"
+	include	"mars/sprites/converted/089.asm"
 	align	4,$FF
 MarsSpr_090:
-	include	"mars/sprites/090.asm"
+	include	"mars/sprites/converted/090.asm"
 	align	4,$FF
 MarsSpr_091:
-	include	"mars/sprites/091.asm"
+	include	"mars/sprites/converted/091.asm"
 	align	4,$FF
 MarsSpr_092:
-	include	"mars/sprites/092.asm"
+	include	"mars/sprites/converted/092.asm"
 	align	4,$FF
 MarsSpr_093:
-	include	"mars/sprites/093.asm"
+	include	"mars/sprites/converted/093.asm"
 	align	4,$FF
 MarsSpr_094:
-	include	"mars/sprites/094.asm"
+	include	"mars/sprites/converted/094.asm"
 	align	4,$FF
 MarsSpr_095:
-	include	"mars/sprites/095.asm"
+	include	"mars/sprites/converted/095.asm"
 	align	4,$FF
 MarsSpr_057:
-	include	"mars/sprites/057.asm"
+	include	"mars/sprites/converted/057.asm"
 	align	4,$FF
 MarsSpr_096:
-	include	"mars/sprites/096.asm"
+	include	"mars/sprites/converted/096.asm"
 	align	4,$FF
 MarsSpr_098:
-	include	"mars/sprites/098.asm"
+	include	"mars/sprites/converted/098.asm"
 	align	4,$FF
 MarsSpr_099:
-	include	"mars/sprites/099.asm"
+	include	"mars/sprites/converted/099.asm"
 	align	4,$FF
 MarsSpr_102:
-	include	"mars/sprites/102.asm"
+	include	"mars/sprites/converted/102.asm"
 	align	4,$FF
 MarsSpr_103:
-	include	"mars/sprites/103.asm"
+	include	"mars/sprites/converted/103.asm"
 	align	4,$FF
 MarsSpr_106:
-	include	"mars/sprites/106.asm"
+	include	"mars/sprites/converted/106.asm"
 	align	4,$FF
 MarsSpr_107:
-	include	"mars/sprites/107.asm"
+	include	"mars/sprites/converted/107.asm"
 	align	4,$FF
 MarsSpr_075:
-	include	"mars/sprites/075.asm"
+	include	"mars/sprites/converted/075.asm"
 	align	4,$FF
 MarsSpr_108:
-	include	"mars/sprites/108.asm"
+	include	"mars/sprites/converted/108.asm"
 	align	4,$FF
 MarsSpr_110:
-	include	"mars/sprites/110.asm"
+	include	"mars/sprites/converted/110.asm"
 	align	4,$FF
 MarsSpr_111:
-	include	"mars/sprites/111.asm"
+	include	"mars/sprites/converted/111.asm"
 	align	4,$FF
 MarsSpr_112:
-	include	"mars/sprites/112.asm"
+	include	"mars/sprites/converted/112.asm"
 	align	4,$FF
 MarsSpr_113:
-	include	"mars/sprites/113.asm"
+	include	"mars/sprites/converted/113.asm"
 	align	4,$FF
 MarsSpr_114:
-	include	"mars/sprites/114.asm"
+	include	"mars/sprites/converted/114.asm"
 	align	4,$FF
 
 ; -------------------------------------------------------------------------
